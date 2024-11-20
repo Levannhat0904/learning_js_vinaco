5c6c7a71588cd15a62dd75f06fd39697ceef7a3d
LEVANNHAT <hiamnhatdz203@gmail.com>
2024-11-20 15:28:15 +0700

    done t2 day6

---
+++ tuan2/day6/main.js
@@ -0,0 +1,4 @@
+import toUpperCase, {tong,hieu} from './modules.js';
+console.log(toUpperCase('dddd'));
+console.log(tong(4,5));
+console.log(hieu(4,5));
---
+++ tuan2/day6/modules.js
@@ -0,0 +1,12 @@
+const sum = function sum(a, b) {
+    return a + b;
+}
+const minus = function minus(a, b) {
+    return a - b;
+}
+function toUpperCase(txt){
+    return txt.toUpperCase();
+}
+export default toUpperCase;
+export const tong = sum;
+export const hieu = minus;
